1d02d658f8809d034fb3519d96b145b76c1fd003
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-08 18:40:14 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,39 @@
 #required packages
-sudo apt install libcairo2-dev
-sudo apt install libjpeg-turbo8-dev
-sudo apt install libpng12-dev
-sudo apt install libtool-bin
-sudo apt install uuid-dev
+sudo apt install libcairo2-dev -y
+sudo apt install libjpeg-turbo8-dev -y
+sudo apt install libpng12-dev -y
+sudo apt install libtool-bin -y
+sudo apt install uuid-dev -y
 #feature packages
-sudo apt install libavcodec-dev
-sudo apt install libavformat-dev
-sudo apt install libavutil-dev
-sudo apt install libswsccale-dev
-sudo apt install freerdp2-dev
-sudo apt install libpango1.0-dev
-sudo apt install libssh2-1-dev
-sudo apt install libtelnet-dev
-sudo apt install libvncserver-dev
-sudo apt install libwebsockets-dev
-sudo apt install libpulse-dev
-sudo apt install libssl-dev
-sudo apt install libvorbis-dev
-sudo apt install libwebp-dev
+sudo apt install libavcodec-dev -y
+sudo apt install libavformat-dev -y
+sudo apt install libavutil-dev -y
+sudo apt install libswsccale-dev -y
+sudo apt install freerdp2-dev -y
+sudo apt install libpango1.0-dev -y
+sudo apt install libssh2-1-dev -y
+sudo apt install libtelnet-dev -y
+sudo apt install libvncserver-dev -y
+sudo apt install libwebsockets-dev -y
+sudo apt install libpulse-dev -y
+sudo apt install libssl-dev -y
+sudo apt install libvorbis-dev -y
+sudo apt install libwebp-dev -y
+#Pull Latest Guacamole Server
 git clone git://github.com/apache/guacamole-server.git
 cd guacamole-server
+#Compile Latest Guacamole Server
 autoreconf -fi
 ./configure --with-init-dir=/etc/init.d
 make
+#install
 make install
 ldconfig
+#creates a .war file to install
 mvn package
+#copies the .war file to tomcat's directory
 cp guacamole/target/guacamole-1.4.0.war /var/lib/tomcat/webapps/guacamole.war
+#restart tomcat
 /etc/init.d/tomcat7 restart
+#start guacamole
 /etc/init.d/guacd start
