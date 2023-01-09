b11d1a6b8a2c8a78744c93b70bae9bfad75153fd
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-08 19:34:58 -0600

    added -y tag to line 4

--- installdocker.sh
+++ installdocker.sh
@@ -1,7 +1,7 @@
 #install docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
